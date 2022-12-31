cec6302eefa7ab8ad6b6ccb53f78f5fe7b8ac5a8
vinay kumar pandey <tellvinay2003@yahoo.co.in>
2023-01-01 04:15:04 +0530

    first Code base

--- API/Program.cs
+++ API/Program.cs
@@ -1,21 +1,29 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => {
+builder.Services.AddDbContext<DataContext>(opt =>
+{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 // // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseCors(Options =>
+{
+    Options.AllowAnyHeader()
+           .AllowAnyMethod()
+           .WithOrigins("http://localhost:4200");
+});
 // // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
 // {
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule // added new module httpClient which will help to consume Http based backend service
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
